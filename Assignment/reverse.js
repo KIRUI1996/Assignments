cda160ec2e50dc079680069ee91c1a5f93882fcb
Victor Kosgei Kirui <79073793+KIRUI1996@users.noreply.github.com>
2023-06-19 14:56:35 +0300

    Add files via upload

---
+++ Assignment/reverse.js
@@ -0,0 +1,4 @@
+let languages = ["c++","java","php"];
+let reversedArray = languages.reverse();
+console.log("Reversed Array: ", reversedArray);
+
---
+++ Assignment/square.js
@@ -0,0 +1,8 @@
+function isPerfectSquere(number) {
+       let sqrt = Math.sqrt(number)
+       if (sqrt%1 == 0) {
+           console.log(true,sqrt)
+       }
+       else console.log(false,sqrt)
+   }
+   console.log(25);
